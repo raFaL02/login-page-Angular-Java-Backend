8974b7947ac8395420f37171a79983df856fe95c
Rafael de Lima <rafalimabcl@gmail.com>
2024-09-20 14:21:22 -0300

    feat: add Spring Boot Application package to use PasswordEncoder Bean in another package.

--- src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
+++ src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.login_auth_api.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@SpringBootApplication(scanBasePackages = {"com.example.login_auth_api.infra.security"})
 public class SecurityConfig {
 
     @Autowired
