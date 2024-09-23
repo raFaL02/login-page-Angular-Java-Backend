38edcdb1fb7d03b7d2e27345f39d9d25bd5c3db6
Rafael de Lima <rafalimabcl@gmail.com>
2024-09-23 00:19:11 -0300

    fix: fix token service error

--- src/main/java/com/example/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.example.login_auth_api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.login_auth_api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -41,7 +42,7 @@ public class TokenService {
                     .build()
                     .verify(token)
                     .getSubject();
-        } catch (JWTCreationException exception) {
+        } catch (JWTVerificationException exception) {
             return null;
         }
     }
